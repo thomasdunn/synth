1d3898b1a9b9f20a4fdf155d993ef548b28ce900
Tom Dunn <tdunn@artofcontext.com>
2016-09-18 22:51:59 -0400

    Added range input to control the gain.

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+var gainRange = document.getElementById('gain');
+
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
 var oscillator = audioCtx.createOscillator();
-var gain = audioCtx.createGain();
+var gainNode = audioCtx.createGain();
 
-oscillator.connect(gain);
-gain.connect(audioCtx.destination);
+oscillator.connect(gainNode);
+gainNode.connect(audioCtx.destination);
 
 oscillator.type = 'triangle';
 oscillator.frequency.value = 185;
 oscillator.start();
+
+gainRange.addEventListener('input', function() {
+  gainNode.gain.value = parseFloat(gainRange.value);
+});
