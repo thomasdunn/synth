e5ac40da0c0fe05d7be798dbb9d6086b9da8bf09
Tom Dunn <tdunn@artofcontext.com>
2016-09-24 00:37:18 -0400

    Added library canvasPlot.js for its oscilloscope and spectrum analyzer code.  Adding initial, essentially unchanged copies of oscilloscope demo script/html.

---
+++ lib/canvasPlot.js/README.md
@@ -0,0 +1,32 @@
+# canvasPlot.js
+Minimal HTML5 Canvas/JavaScript 2D data plotting library.
+
+I am too lazy to explain hows and whys, just take a look at the example files:
+* plot.html - basic data plotting;
+* gridAutoLinear.html - automatic grid for linear maps;
+* gridAutoSemiLogX.html - automatic grid for semilogx maps;
+* resample.html - linear resample;
+* spectrum.html - spectrum analyzer:
+* scope.html - oscilloscope.
+
+The experimental folder contains stuff that I just didn't want to throw away.
+
+## TODO
+
+* grid ticks, axis labels, etc.;
+* higher-order interpolators and resamplers;
+* idiotic APIs for the laziest;
+* serious API review;
+* serious documentation;
+* the HTML/CSS is probably somewhat broken;
+* minified version;
+* releases/stable APIs?
+* Node.js package?
+* split scope/spectrum analyser in a separate project?
+* time persistence of scope signals?
+* analysis of continuous audio streams (ScriptProcessorNode, AudioWorker,
+  Transferable Objects, etc.)?
+* measuring?
+* different line drawers (points, areas, etc.)?
+* 3D (WebGL)?
+* optimizations?
---
+++ oscilloscope.js
@@ -0,0 +1,183 @@
+function init() {
+	var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+	var osc = audioCtx.createOscillator();
+	var gainIn = audioCtx.createGain();
+	var analyser = audioCtx.createAnalyser();
+	var gainOut = audioCtx.createGain();
+
+	osc.connect(gainIn);
+	gainIn.connect(analyser);
+	analyser.connect(gainOut);
+	gainOut.connect(audioCtx.destination);
+
+	var canvas = document.getElementById("canvas");
+
+	var samples = Object.create(canvasPlot.samples);
+	samples.init(new Float32Array(32768), new Float32Array(32768));
+
+	var signal = Object.create(canvasPlot.scopeSignal);
+	signal.init(samples, null);
+	signal.analyser = analyser;
+
+	var resampler = Object.create(canvasPlot.resamplerLinear);
+	resampler.init();
+
+	var scope = Object.create(canvasPlot.simpleScopeWidget);
+	scope.init(canvas, null, [signal], null,
+		Object.create(canvasPlot.curveDrawerLinear));
+
+	function setValElement(name) {
+		var elem = document.getElementById(name);
+		elem.valElement = document.getElementById(name + "_val");
+		if (elem.valElement.innerHTML != elem.value)
+			elem.valElement.innerHTML = elem.value;
+		return elem;
+	}
+	var frequency_elem = setValElement("frequency");
+	var time_elem = setValElement("time");
+	var y1_elem = setValElement("y1");
+	var y2_elem = setValElement("y2");
+	var triggerLevel_elem = setValElement("triggerLevel");
+	var points_elem = setValElement("points");
+	var volume_in_elem = setValElement("volume_in");
+	var volume_out_elem = setValElement("volume_out");
+
+	var fftSize = Math.ceil(Math.log2(0.001 * time_elem.max
+					  * audioCtx.sampleRate));
+	if (fftSize < 5)
+		fftSize = 5;
+	else if (fftSize > 15)
+		fftSize = 15;
+	analyser.fftSize = 1 << fftSize;
+	signal.updateAnalyser();
+
+	osc.frequency.value = parseFloat(frequency_elem.value);
+	scope.scope.time = parseFloat(time_elem.value) * 0.001;
+	scope.scope.yRange.min = parseFloat(y1_elem.value);
+	scope.scope.yRange.max = parseFloat(y2_elem.value);
+	signal.triggerLevel = parseFloat(triggerLevel_elem.value);
+	scope.scope.resamplerPoints = parseInt(points_elem.value);
+	gainIn.gain.value = parseFloat(volume_in_elem.value) * 0.01;
+	gainOut.gain.value = parseFloat(volume_out_elem.value) * 0.01;
+
+	function grid_change () {
+		scope.scope.showGrid = this.value == "visible";
+		scope.update(false);
+	}
+	var grid_elems = document.getElementsByName("grid");
+	for (var i = 0; i < grid_elems.length; i++) {
+		grid_elems[i].addEventListener("change", grid_change);
+		if (grid_elems[i].checked)
+			scope.scope.showGrid = grid_elems[i].value == "visible";
+	}
+
+	function trigger_change () {
+		signal.trigger = canvasPlot.scopeSignal.triggerType[this.value];
+	}
+	var trigger_elems = document.getElementsByName("trigger");
+	for (var i = 0; i < trigger_elems.length; i++) {
+		trigger_elems[i].addEventListener("change", trigger_change);
+		if (trigger_elems[i].checked)
+			signal.trigger = canvasPlot.scopeSignal
+					 .triggerType[trigger_elems[i].value];
+	}
+
+	function resampler_change () {
+		scope.scope.resampler = this.value == "linear" ? resampler
+							       : null;
+	}
+	var resampler_elems = document.getElementsByName("resampler");
+	for (var i = 0; i < resampler_elems.length; i++) {
+		resampler_elems[i].addEventListener("change", resampler_change);
+		if (resampler_elems[i].checked)
+			scope.scope.resampler =
+				resampler_elems[i].value == "linear" ? resampler
+								     : null;
+	}
+
+	function waveform_change () {
+		osc.type = this.value;
+	}
+	var waveform_elems = document.getElementsByName("waveform");
+	for (var i = 0; i < waveform_elems.length; i++) {
+		waveform_elems[i].addEventListener("change", waveform_change);
+		if (waveform_elems[i].checked)
+			osc.type = waveform_elems[i].value;
+	}
+
+	scope.update(false);
+
+	function input() {
+		if (this.valElement.innerHTML == this.value)
+			return;
+		this.valElement.innerHTML = this.value;
+
+		switch (this) {
+			case frequency_elem:
+				osc.frequency.value = parseFloat(this.value);
+				break;
+			case time_elem:
+				scope.scope.time = parseFloat(this.value)
+						   * 0.001;
+				scope.update(false);
+				break;
+			case y1_elem:
+				scope.scope.yRange.min = parseFloat(this.value);
+				scope.update(false);
+				break;
+			case y2_elem:
+				scope.scope.yRange.max = parseFloat(this.value);
+				scope.update(false);
+				break;
+			case triggerLevel_elem:
+				signal.triggerLevel = parseFloat(this.value);
+				break;
+			case points_elem:
+				scope.scope.resamplerPoints =
+					parseInt(this.value);
+				break;
+			case volume_in_elem:
+				gainIn.gain.value = parseFloat(this.value)
+						    * 0.01;
+				break;
+			case volume_out_elem:
+				gainOut.gain.value = parseFloat(this.value)
+						     * 0.01;
+				break;
+		}
+	}
+	frequency_elem.addEventListener("input", input);
+	time_elem.addEventListener("input", input);
+	y1_elem.addEventListener("input", input);
+	y2_elem.addEventListener("input", input);
+	triggerLevel_elem.addEventListener("input", input);
+	points_elem.addEventListener("input", input);
+	volume_in_elem.addEventListener("input", input);
+	volume_out_elem.addEventListener("input", input);
+
+	osc.start();
+
+	var ctx = canvas.getContext("2d");
+
+	var lastTime;
+	var fps = 0;
+	function draw() {
+		requestAnimationFrame(draw);
+		scope.updateSignals();
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		scope.draw();
+
+		var curTime = Date.now();
+		if (lastTime) {
+			fps = 0.1 * 1000 / (curTime - lastTime) + 0.9 * fps;
+			ctx.fillStyle = "black";
+			ctx.font = "16px sans";
+			ctx.fillText("FPS: " + fps.toFixed(2), 10, 26);
+		}
+		lastTime = curTime;
+	}
+	draw();
+}
+
+window.addEventListener("load", init);
